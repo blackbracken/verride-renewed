036946524d31ea81480e88b768a5ae7fbc513196
blackbracken <me@bracken.black>
2025-05-01 01:53:04 +0900

    refactor: rename ConnectionList → ConnectionSet

--- src/main/java/black/bracken/verriderenewed/feature/piggyback/ConnectionSet.java
+++ src/main/java/black/bracken/verriderenewed/feature/piggyback/ConnectionSet.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
-public final class ConnectionList {
+public final class ConnectionSet {
 
     private final Map<ConnectorId, Connection> connectionMap = new ConcurrentHashMap<>();
 
